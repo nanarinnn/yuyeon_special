4b9f2ca50973c206ca7af4672a10e231bef5fdb0
nanarinnn <sakuchann00125@gmail.com>
2026-07-29 00:16:09 +0900

    Update cardImages.ts

--- src/data/cardImages.ts
+++ src/data/cardImages.ts
@@ -2,7 +2,7 @@
 // Set frontImageUrl and backImageUrl to "image" as requested.
 // You can edit "image" with your image URL or relative path directly on GitHub!
 
-export const BACK_SUMMER_IMAGE = "yuyeon_special/public/image/back_summer.png";
+export const BACK_SUMMER_IMAGE = "https://raw.githubusercontent.com/nanarinnn/yuyeon_special/main/image/back_summer.png";
 
 export interface CardItemData {
   id: number;
@@ -17,42 +17,42 @@ export const CARD_FLIP_ITEMS: CardItemData[] = [
     id: 1,
     title: 'Summer Special #1',
     orientation: 'vertical',
-    frontImageUrl: 'yuyeon_special/public/image/front1_summer.png',
+    frontImageUrl: 'https://raw.githubusercontent.com/nanarinnn/yuyeon_special/main/image/front1_summer.png',
     backImageUrl: BACK_SUMMER_IMAGE,
   },
   {
     id: 2,
     title: 'Summer Special #2',
     orientation: 'vertical',
-    frontImageUrl: 'yuyeon_special/public/image/front2_summer.png',
+    frontImageUrl: 'https://raw.githubusercontent.com/nanarinnn/yuyeon_special/main/image/front2_summer.png',
     backImageUrl: BACK_SUMMER_IMAGE,
   },
   {
     id: 3,
     title: 'Summer Special #3',
     orientation: 'horizontal',
-    frontImageUrl: 'yuyeon_special/public/image/front3_summer.png',
+    frontImageUrl: 'https://raw.githubusercontent.com/nanarinnn/yuyeon_special/main/image/front3_summer.png',
     backImageUrl: BACK_SUMMER_IMAGE,
   },
   {
     id: 4,
     title: 'Summer Special #4',
     orientation: 'horizontal',
-    frontImageUrl: 'yuyeon_special/public/image/front4_summer.png',
+    frontImageUrl: 'https://raw.githubusercontent.com/nanarinnn/yuyeon_special/main/image/front4_summer.png',
     backImageUrl: BACK_SUMMER_IMAGE,
   },
   {
     id: 5,
     title: 'Summer Special #5',
     orientation: 'horizontal',
-    frontImageUrl: 'yuyeon_special/public/image/front5_summer.png',
+    frontImageUrl: 'https://raw.githubusercontent.com/nanarinnn/yuyeon_special/main/image/front5_summer.png',
     backImageUrl: BACK_SUMMER_IMAGE,
   },
   {
     id: 6,
     title: 'Summer Special #6',
     orientation: 'horizontal',
-    frontImageUrl: 'yuyeon_special/public/image/front_summer.png',
+    frontImageUrl: 'https://raw.githubusercontent.com/nanarinnn/yuyeon_special/main/image/front6_summer.png',
     backImageUrl: BACK_SUMMER_IMAGE,
   },
 ];
